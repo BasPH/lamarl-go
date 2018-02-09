7d2d8933d7ff7423354ea6e77d6dc3125c0ba3d3
Bas Harenslak <basharenslak@gmail.com>
2018-02-09 14:09:12 +0100

    Change package

--- main_test.go
+++ main_test.go
@@ -1,26 +1,25 @@
-package main_test
+package main
 
 import (
-	"github.com/BasPH/lamarl-go"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestHandler(t *testing.T) {
 	tests := []struct {
-		request main.SimulationInput
+		request SimulationInput
 		expect  string
 		err     error
 	}{
 		{
-			request: main.SimulationInput{20, []string{"1", "2", "3"}},
+			request: SimulationInput{20, []string{"1", "2", "3"}},
 			expect:  "{\"nsimulations\":20,\"order\":[\"1\",\"2\",\"3\"]}",
 			err:     nil,
 		},
 	}
 
 	for _, test := range tests {
-		response, err := main.Handler(test.request)
+		response, err := Handler(test.request)
 		assert.IsType(t, test.err, err)
 		assert.Equal(t, test.expect, response.Body)
 	}
