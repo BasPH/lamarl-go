9cebdfc963a9e14c80e3335608889861dbf84160
Bas Harenslak <basharenslak@gmail.com>
2018-02-26 23:49:51 +0100

    Add simulation script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Lamarl-Go
 
 Build with `make`. The Makefile automatically updates the Lambda function.
+
+To run simulations, execute the Go file in `cmd/simulate`.
---
+++ cmd/simulate/simulate.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+	"bytes"
+	"io/ioutil"
+	"strconv"
+	"sync"
+	"fmt"
+	"time"
+	"log"
+)
+
+func trackTime(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", name, elapsed)
+}
+
+func main() {
+	defer trackTime(time.Now(), "main()")
+	url := "https://azj3z8mlq6.execute-api.eu-west-1.amazonaws.com/Prod/"
+	cardOrder := []byte(`{"order": ["maki-1", "maki-2", "maki-3", "sashimi", "egg", "salmon", "squid", "wasabi", "pudding", "tempura", "dumpling", "tofu", "eel", "temaki"]}`)
+	nRequests := 1000
+
+	var wg sync.WaitGroup
+	wg.Add(nRequests)
+	results := make([]int, nRequests)
+
+	for i := 0; i < nRequests; i++ {
+		go func(url string, cardOrder []byte, i int) {
+			defer wg.Done()
+			request := bytes.NewBuffer(cardOrder)
+			result, err := http.Post(url, "application/json", request)
+			if err != nil {
+				fmt.Println("Error: %v", err)
+			} else {
+				body, err := ioutil.ReadAll(result.Body)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					r, _ := strconv.Atoi(string(body))
+					results[i] = r
+				}
+			}
+		}(url, cardOrder, i)
+	}
+
+	wg.Wait()
+	//fmt.Println(results)
+}
